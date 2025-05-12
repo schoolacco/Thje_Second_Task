3f443deb593f6597bd0c11ac5c3427e46f21a533
schoolacco <anthony.revel510@education.nsw.gov.au>
2025-05-13 08:37:10 +1000

    Fixed Github stash stuff

--- Module.py
+++ Module.py
@@ -14,8 +14,6 @@ class Gamble:
               return "Success"
       else:
           return "Failure"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
   def Rng(collection, luck):
     items = [
         (1000000000, "MAINFRAME"),
@@ -28,30 +26,5 @@ class Gamble:
             return  # Stop rolling on success
     # If no success after all attempts
     collection["Item1"] = collection.get("Item1", 0) + 1
-
-=======
-=======
->>>>>>> Stashed changes
-  def Check_Success(collection,luck,chance,name):
-      var1 = "Failure"
-      count = 0
-      while var1 != "Success" and count <= 4:
-          Item = Gamble(chance,name)
-          var1 = Gamble.insert(collection,luck,Item.chance, Item.name)
-          count += 1
-  def Rng(collection, luck):
-      var1 = Gamble.insert(collection, luck, 1000000000, "MAINFRAME")
-      var2 = Gamble.insert(collection, luck, 1000, "Item2")
-      Item = Gamble(10,"Item3")
-      var3 = Gamble.insert(collection, luck, Item.chance, Item.name)
-      if var1 == "Failure" or var2 == "Failure" or var3 == "Failure":
-         if "Item1" not in collection:
-             collection["Item1"] = 1
-         else:
-          collection["Item1"] += 1
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
   def placeholder_function(collection):
       print(collection)
