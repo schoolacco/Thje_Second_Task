fc4a52efb3934cbaf258f54ab32402c58d1efecc
schoolacco <anthony.revel510@education.nsw.gov.au>
2025-05-13 08:34:36 +1000

    Needed to get rid of this stash

--- Module.py
+++ Module.py
@@ -14,6 +14,7 @@ class Gamble:
               return "Success"
       else:
           return "Failure"
+<<<<<<< Updated upstream
   def Rng(collection, luck):
     items = [
         (1000000000, "MAINFRAME"),
@@ -27,5 +28,24 @@ class Gamble:
     # If no success after all attempts
     collection["Item1"] = collection.get("Item1", 0) + 1
 
+=======
+  def Check_Success(collection,luck,chance,name):
+      var1 = "Failure"
+      count = 0
+      while var1 != "Success" and count <= 4:
+          Item = Gamble(chance,name)
+          var1 = Gamble.insert(collection,luck,Item.chance, Item.name)
+          count += 1
+  def Rng(collection, luck):
+      var1 = Gamble.insert(collection, luck, 1000000000, "MAINFRAME")
+      var2 = Gamble.insert(collection, luck, 1000, "Item2")
+      Item = Gamble(10,"Item3")
+      var3 = Gamble.insert(collection, luck, Item.chance, Item.name)
+      if var1 == "Failure" or var2 == "Failure" or var3 == "Failure":
+         if "Item1" not in collection:
+             collection["Item1"] = 1
+         else:
+          collection["Item1"] += 1
+>>>>>>> Stashed changes
   def placeholder_function(collection):
       print(collection)
