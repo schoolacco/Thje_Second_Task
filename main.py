7e076e62cdb7b693ac7b98793411f8494ebefb0c
schoolacco <anthony.revel510@education.nsw.gov.au>
2025-05-15 10:36:16 +1000

    Attempted to convert into a ttk notebook
    I attempted to insert it into a ttk notebook and attempted to add music, ran to strange issue with it not recognising my file.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk, Image
 from Module import Gamble
+from pydub import AudioSegment
+from pydub.playback import play
+import pyperclip
 collection = {}
 luck = 1
 root = Tk()
@@ -13,8 +17,20 @@ root.geometry("500x500+20+120")
 ico = Image.open('dice.png')
 photo = ImageTk.PhotoImage(ico)
 root.wm_iconphoto(False, photo)
-Label(root, text="There is nothing much to say, click the button to begin.", bg="black", fg="white", anchor="center").pack()
-roll = Button(root, text="Roll", command=lambda: Gamble.Rng(collection, luck), bg="black", fg="white")
-roll.place(relx=0.5, rely=0.5)
-Button(root, text="View collection", command=lambda: Gamble.placeholder_function(collection), bg="black", fg="white").pack()
+try:
+  song = AudioSegment.from_mp3("Fallen_Symphony.mp3")
+  play(song)
+except Exception as e:
+  pyperclip.copy(e)
+Nb = ttk.Notebook(root, cursor="circle")
+s = ttk.Style()
+s.configure('Apod_frame.TFrame', background="black") #Change Style() to create bgs for frames
+rng_frame = ttk.Frame(Nb, width=2000, height=2000, style='Apod_frame.TFrame') #Create a tab in the notebook
+#rng = Frame(Nb,height=2000, width=2000,, name="rng")
+Label(rng_frame, text="There is nothing much to say, click the button to begin.", bg="black", fg="white", anchor="center").pack()
+roll = Button(rng_frame, text="Roll", command=lambda: Gamble.Rng(collection, luck), bg="black", fg="white")
+roll.pack()
+Button(rng_frame, text="View collection", command=lambda: Gamble.placeholder_function(collection), bg="black", fg="white").pack()
+rng_frame.pack()
+Nb.pack()
 root.mainloop()
